d611211188a4961a2e94f31bfe5fbd293a2b1bfc
Lan00079 <55729942+Lan00079@users.noreply.github.com>
2026-08-27 14:38:56 +0800

    site: sw.js = exact dashboard file (3nbf4 MultiPush zone 11663706) for install check

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11665414
+    "domain": "3nbf4.com",
+    "zoneId": 11663706
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
